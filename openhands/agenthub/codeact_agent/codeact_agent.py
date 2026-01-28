4fc1379462a562f38bf7ca88ddb6bbed488045ea
Brian Yu <bxyu@nvidia.com>
2026-01-27 20:40:59 -0800

    add tags

--- openhands/agenthub/codeact_agent/codeact_agent.py
+++ openhands/agenthub/codeact_agent/codeact_agent.py
@@ -266,7 +266,7 @@ class CodeActAgent(Agent):
         }
 
         # TODO remove
-        logger.error(params)
+        logger.error("-" * 40 + "\nPARAMS", params)
 
         from nemo_gym.server_utils import get_response_json, raise_for_status
 
@@ -292,7 +292,7 @@ class CodeActAgent(Agent):
             }
 
         # TODO remove
-        logger.error(response)
+        logger.error("-" * 40 + "\nRESPONSE"response)
 
         # Restore the removed token fields to messages
         for i, fields in removed_fields.items():
