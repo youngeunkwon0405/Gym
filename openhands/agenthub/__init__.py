d05dbde4c655627f7ce6e2b665e8ee829f01a492
Sugam Devare <sdevare@nvidia.com>
2026-02-12 18:52:15 -0800

    feat: register opecodeagentt

--- openhands/agenthub/__init__.py
+++ openhands/agenthub/__init__.py
@@ -8,6 +8,7 @@ from openhands.agenthub import (  # noqa: E402
     codeact_agent,
     dummy_agent,
     loc_agent,
+    opencode_agent,
     readonly_agent,
     visualbrowsing_agent,
 )
@@ -21,4 +22,5 @@ __all__ = [
     'visualbrowsing_agent',
     'readonly_agent',
     'loc_agent',
+    'opencode_agent',
 ]
